faba21e394ee0b0337cdcc103d2ef13fa10770de
Nirav <nirav.16bca@gmail.com>
2023-10-18 10:11:37 +0530

    delete params.row pros

--- src/components/core/permission/Permission.tsx
+++ src/components/core/permission/Permission.tsx
@@ -123,7 +123,7 @@ const Permission = () => {
       renderCell: (params) => {
         return (
           <Box>
-            <Checkbox checked={params.row} />
+            <Checkbox />
           </Box>
         );
       },
